7d1d1152c8cfe8bcd7f1b8df44bbbed577e549e8
jpcp0614 <jpcp0614@gmail.com>
2023-08-22 19:39:15 -0300

    :sparkles: feat - Add component StartScreen

---
+++ src/components/StartScreen.jsx
@@ -0,0 +1,13 @@
+import './StartScreen.css';
+
+const StartScreen = () => {
+	return (
+		<div className="start">
+			<h1>Secret World</h1>
+			<p>Click the button to play</p>
+			<button>Play</button>
+		</div>
+	);
+};
+
+export default StartScreen;
