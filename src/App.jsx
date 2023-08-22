ade8993dfe2eba95b4b3509f7e6b626e080cc37b
jpcp0614 <jpcp0614@gmail.com>
2023-08-22 20:12:00 -0300

    :sparkles: feat - Add Stages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
+// CSS
 import './App.css';
+
+// React
+import { useState } from 'react';
+
+// data
+import { wordsList } from './data/words';
+
+// Components
 import StartScreen from './components/StartScreen';
 
+const STAGES = [
+	{ id: 1, name: 'start' },
+	{ id: 2, name: 'game' },
+	{ id: 3, name: 'end' },
+];
+
 function App() {
+	const [gameStage, setGameStage] = useState(STAGES[0].name);
+
 	return (
 		<div className="App">
-			<StartScreen />
+			{gameStage === 'start' && <StartScreen />}
+			{gameStage === 'game' && <StartScreen />}
+			{gameStage === 'end' && <StartScreen />}
 		</div>
 	);
 }
