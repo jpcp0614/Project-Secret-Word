f2992d48651e3880164fddc2bb5cddccc3a782c1
jpcp0614 <jpcp0614@gmail.com>
2023-08-23 00:41:33 -0300

    :sparkles: feat - add UseEffect and useCallback

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import './App.css';
 
 // React
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 // data
 import { wordsList } from './data/words';
@@ -31,7 +31,7 @@ function App() {
 	const [guesses, setGuesses] = useState(GUESSESQTY);
 	const [score, setScore] = useState(0);
 
-	const pickWordAndCategory = () => {
+	const pickWordAndCategory = useCallback(() => {
 		// pick a random category
 		const categories = Object.keys(words);
 		const numberOfKeys = Object.keys(categories).length;
@@ -42,11 +42,14 @@ function App() {
 			words[category][Math.floor(Math.random() * words[category].length)];
 		// words[category].length) = quantidade de palavras na categoria
 		return { word, category };
-	};
+	}, [words]);
 
 	// start secret word game
-	const startGame = () => {
-		//pick word and pick category
+	const startGame = useCallback(() => {
+		// clear all letters
+		clearLetterStates();
+
+		// pick word and pick category
 		const { word, category } = pickWordAndCategory();
 
 		// create array of letters
@@ -61,7 +64,7 @@ function App() {
 		setPickedCategory(category);
 		setLetters(wordLetters);
 		setGameStage(STAGES[1].name);
-	};
+	}, [pickWordAndCategory]);
 
 	// check if letter has already been utilized
 	const checkLetterAlreadyUtilized = (letter) => {
@@ -96,16 +99,30 @@ function App() {
 	const clearLetterStates = () => {
 		setGuessedLetters([]);
 		setWrongLetters([]);
-	}
+	};
 
-	// monitorar o estado das guesses
+	// check loose condition
 	useEffect(() => {
 		if (guesses <= 0) {
 			// reset all states
 			clearLetterStates();
-			setGameStage(STAGES[2].name)
+			setGameStage(STAGES[2].name);
 		}
 	}, [guesses]);
+
+	// check win condition
+	useEffect(() => {
+		const uniqueLetters = [...new Set(letters)]; // array de letras únicas
+
+		// win condition
+		if (guessedLetters.length === uniqueLetters.length) {
+			// add score
+			setScore((actualScore) => (actualScore += 100));
+
+			// restart game with new record
+			startGame();
+		}
+	}, [guessedLetters, letters, startGame]);
 
 	// reinicia o jogo
 	const restartGame = () => {
@@ -130,7 +147,9 @@ function App() {
 			{gameStage === 'game' && (
 				<Game states={states} verifyLetter={verifyLetter} />
 			)}
-			{gameStage === 'end' && <GameOver restartGame={restartGame} score={score}/>}
+			{gameStage === 'end' && (
+				<GameOver restartGame={restartGame} score={score} />
+			)}
 		</div>
 	);
 }
