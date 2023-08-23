af603766ef65a61361ae783857ac8a6550b9fa9d
jpcp0614 <jpcp0614@gmail.com>
2023-08-22 21:26:25 -0300

    :sparkles: feat - Add structure in component

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -3,9 +3,30 @@ import './Game.css';
 
 const Game = ({ verifyLetter }) => {
 	return (
-		<div>
-			<h1>Game</h1>
-			<button onClick={verifyLetter}>End</button>
+		<div className="game">
+			<p className="points">
+				<span>Score: 000</span>
+			</p>
+			<h1>Guess the word</h1>
+			<h3>
+				Any tip? <span>TIP</span>{' '}
+			</h3>
+			<div className="wordContainer">
+				<span className="letter">A</span>
+				<div className="blankSquare"></div>
+			</div>
+			<div className="letterContainer">
+				<p>Try to guess a letter:</p>
+				<form>
+					<input type="text" name="letter" maxLength="1" required />
+					<button>Play</button>
+				</form>
+			</div>
+			<div className="wrongLettersContainer">
+				<p>Letters already chosen</p>
+        <span>a, </span>
+        <span>b, </span>
+			</div>
 		</div>
 	);
 };
